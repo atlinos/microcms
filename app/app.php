156dad8e8eb0607150dd29074c03542465fb2367
atlinos <jp.atlinos@hotmail.fr>
2020-05-17 22:45:35 +0200

    Iteration 5 - Twig integration for views

--- app/app.php
+++ app/app.php
@@ -7,6 +7,9 @@ ErrorHandler::register();
 ExceptionHandler::register();
 
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/../views',
+]);
 
 $app['dao.article'] = function ($app) {
     return new MicroCMS\DAO\ArticleDAO($app['db']);
--- app/routes.php
+++ app/routes.php
@@ -3,8 +3,5 @@
 $app->get('/', function () use($app) {
     $articles = $app['dao.article']->findAll();
 
-    ob_start();
-    require '../views/view.php';
-    $view = ob_get_clean();
-    return $view;
+    return $app['twig']->render('index.html.twig', ['articles' => $articles]);
 });
