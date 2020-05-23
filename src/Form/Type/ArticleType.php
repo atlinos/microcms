23e92de5e4bb36ec2bf6fe5fd1d4504c9291020c
atlinos <jp.atlinos@hotmail.fr>
2020-05-23 12:35:30 +0200

    Iteration 14 - Bootstrap Twitter and form validation

--- src/Form/Type/ArticleType.php
+++ src/Form/Type/ArticleType.php
@@ -8,14 +8,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('content', TextareaType::class);
+            ->add('title', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 5, 'max' => 100])
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'required' => true,
+                'constraints' => new Assert\NotBlank()
+            ]);
     }
 
     public function getName()
--- src/Form/Type/CommentType.php
+++ src/Form/Type/CommentType.php
@@ -5,14 +5,34 @@ namespace MicroCMS\Form\Type;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('content', TextareaType::class);
+        $builder
+//            ->add('email', EmailType::class, [
+//                'label' => 'Votre email',
+//                'attr' => ['placeholder' => '(ne sera pas publié)'],
+//                'required' => true,
+//                'invalid_message' => 'Cet email n\'est pas valide',
+//                'constraints' => new Assert\Email(['checkMX' => true])
+//            ])
+//            ->add('website', UrlType::class, [
+//                'label' => 'Your website',
+//                'required' => false,
+//                'constraints' => new Assert\Url()
+//            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Your comment',
+                'required' => true,
+                'constraints' => new Assert\NotBlank()
+            ]);
     }
 
     public function getName()
